4612d494082a0c3a0c859025ccabc2770c50300d
Jason Addleman <jas7457@gmail.com>
2023-04-22 15:41:06 -0400

    Done

--- src/routes/Instructions/Instructions.tsx
+++ src/routes/Instructions/Instructions.tsx
@@ -1,5 +1,34 @@
 import { Container } from '../../components';
 
+import image1 from './image1.png';
+import styles from './Instructions.module.scss';
+
 export function Instructions() {
-	return <Container>Instructions</Container>;
+	return (
+		<Container>
+			<div className={styles.container}>
+				{[
+					{
+						img: image1,
+						instructions: (
+							<p>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi dolore dolorum eius minus doloremque
+								aspernatur illum quam facilis rem voluptate harum reiciendis labore debitis et exercitationem est
+								beatae, mollitia dolor!
+							</p>
+						),
+					},
+					{ img: image1, instructions: <p>Some other text</p> },
+					{ img: image1, instructions: <p>More text</p> },
+				].map((item, index) => (
+					<div key={index} className={styles.wrapper}>
+						<a href={item.img} target="_blank">
+							<img src={item.img} />
+						</a>
+						<div>{item.instructions}</div>
+					</div>
+				))}
+			</div>
+		</Container>
+	);
 }
