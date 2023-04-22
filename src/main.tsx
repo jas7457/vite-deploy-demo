5bb7423e287685fa0a65de5d08be6046b1b43121
Jason Addleman <jas7457@gmail.com>
2023-04-22 13:04:05 -0400

    React router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { Root, About } from './routes';
+import './index.css';
+
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Root />,
+		},
+		{
+			path: '/about',
+			element: <About />,
+		},
+	],
+	{ basename: '/vite-deploy-demo/' }
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
